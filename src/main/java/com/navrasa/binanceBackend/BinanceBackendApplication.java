3c6f3b9b03ebb85f3d2b98395fb0f295e4ab1b4c
ankitdoi--coder <ankitdoi82@gmail.com>
2026-08-04 12:06:47 +0530

    added env

--- src/main/java/com/navrasa/binanceBackend/BinanceBackendApplication.java
+++ src/main/java/com/navrasa/binanceBackend/BinanceBackendApplication.java
@@ -3,10 +3,19 @@ package com.navrasa.binanceBackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class BinanceBackendApplication {
 
 	public static void main(String[] args) {
+		// Load the .env file and push variables into System Properties so Spring can
+		// read them
+		Dotenv.configure()
+				.systemProperties()
+				.ignoreIfMissing() // CRITICAL: Prevents crashing on Render where .env doesn't exist
+				.load();
+
 		SpringApplication.run(BinanceBackendApplication.class, args);
 	}
 
